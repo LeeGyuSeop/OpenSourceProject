9822fb756e3cab1d637cf55c778ff1b5a699e7d1
FoxRain10 <sudo.youngwoo@gmail.com>
2021-06-16 07:53:50 +0900

    역세권 파싱 최종업데이트

--- 소스코드/py/StationParsing.py
+++ 소스코드/py/StationParsing.py
@@ -15,20 +15,24 @@ x=data["X좌표값"]
 y=data["Y좌표값"]
 
 ca=pd.read_csv("D:/GIT_Project/OpenSourceProject/자료분석/상권정보(칸분리).csv",encoding='UTF-8')
-commercialData=ca.loc[(ca["시군구명"]==a)&(ca["상권업종중분류명"]==b),:]
+commercialData=ca.loc[(ca["시군구명"]==a),:]
 staX=commercialData["경도"]
 staY=commercialData["위도"]
 commer=commercialData["상호명"]
 Classification=commercialData["상권업종중분류명"]
+
 for i in range(len(x)):
    a=(float(x.iloc[i]),float(y.iloc[i]))
-   time.sleep(10)
+   selcount=0
+   count=0
    for j in range(len(commercialData)):
-      b=(float(staX.iloc[j]),float(staY.iloc[j]))
+      c=(float(staX.iloc[j]),float(staY.iloc[j]))
       
-      if haversine(a,b)<=0.7:
-          
-         print(station.iloc[i],commer.iloc[j])
-         print(haversine(a,b))
-         print(Classification.iloc[i])
-         
+      if haversine(a,c)<=0.4:
+         if Classification.iloc[j]==b:
+            selcount+=1
+         else :
+            count+=1
+
+   print(station.iloc[i],"역 비율=",count/selcount)   
+      
