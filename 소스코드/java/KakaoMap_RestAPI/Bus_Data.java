7e002ab696d4614a356975dd1243fb4fce2eb377
여우비 <77572677+YOUNGWOO-PARK@users.noreply.github.com>
2021-07-11 14:27:01 +0900

    Update Bus_Data.java

--- 소스코드/java/KakaoMap_RestAPI/Bus_Data.java
+++ 소스코드/java/KakaoMap_RestAPI/Bus_Data.java
@@ -1,21 +1,22 @@
 
 public class Bus_Data {
-	String Code;
-	String Bus;
-	String morning;
-	String lunch;
-	String afternoon;
-	String dinner;
-	String address;
-	Bus_Data(String Code,String Bus,String address,
-			int morning,int lunch,int afternoon,
+    String Code;
+    String Bus;
+    String morning;
+    String lunch;
+    String afternoon;
+    String dinner;
+    String address;
+	
+    Bus_Data(String Code, String Bus, String address,
+			int morning, int lunch, int afternoon,
 			int dinner){
-		this.Code=Code;
-		this.Bus=Bus;
-		this.address=address;
-		this.morning=String.valueOf(morning);
-		this.lunch=String.valueOf(lunch);
-		this.afternoon=String.valueOf(afternoon);
-		this.dinner=String.valueOf(dinner);
-	}
+	this.Code = Code;
+	this.Bus = Bus;
+	this.address = address;
+	this.morning = String.valueOf(morning);
+	this.lunch = String.valueOf(lunch);
+	this.afternoon = String.valueOf(afternoon);
+        this.dinner = String.valueOf(dinner);
+    }
 }
